96b25bd40b7d675f53b6be7a48f0c72613b8188d
Baramykova Jane <baramikovajane@gmail.com>
2026-03-12 21:01:36 +0200

    add hw5

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from "cookie-parser";
-
+import userRoutes from './routes/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
-
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
